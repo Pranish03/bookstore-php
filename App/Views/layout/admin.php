32f40f8388d8561a222f8341a1eb29003fc1ae9c
Pranish Chaulagain <cpranishabc163@gmail.com>
2026-04-21 17:32:05 +0545

    Refactor components and layouts

--- App/Views/layout/admin.php
+++ App/Views/layout/admin.php
@@ -16,6 +16,10 @@
                 <li><a href="/admin/books">Books</a></li>
                 <li><a href="/admin/orders">Orders</a></li>
             </ul>
+
+            <form action="/logout" method="post">
+                <button type="submit">Logout</button>
+            </form>
         </nav>
     </aside>
 
