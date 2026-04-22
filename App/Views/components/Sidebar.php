6d38139775962c4b12eb7683d69b5b0960f6baab
Pranish Chaulagain <cpranishabc163@gmail.com>
2026-04-22 14:22:12 +0545

    Refactor sidebar

---
+++ App/Views/components/Sidebar.php
@@ -0,0 +1,14 @@
+<aside>
+    <nav>
+        <ul>
+            <li><a href="/admin">Dashboard</a></li>
+            <li><a href="/admin/users">Users</a></li>
+            <li><a href="/admin/books">Books</a></li>
+            <li><a href="/admin/orders">Orders</a></li>
+        </ul>
+
+        <form action="/logout" method="post">
+            <button type="submit">Logout</button>
+        </form>
+    </nav>
+</aside>
--- App/Views/layout/admin.php
+++ App/Views/layout/admin.php
@@ -8,20 +8,7 @@
 </head>
 
 <body>
-    <aside>
-        <nav>
-            <ul>
-                <li><a href="/admin">Dashboard</a></li>
-                <li><a href="/admin/users">Users</a></li>
-                <li><a href="/admin/books">Books</a></li>
-                <li><a href="/admin/orders">Orders</a></li>
-            </ul>
-
-            <form action="/logout" method="post">
-                <button type="submit">Logout</button>
-            </form>
-        </nav>
-    </aside>
+    <?= component('Sidebar') ?>
 
     <main>
         <?= $content ?>
